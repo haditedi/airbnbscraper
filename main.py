0b6f7663ef015cb1ab3e60629533cdf5ab58e93f
Hadi Tedi <haditedi@gmail.com>
2024-08-12 13:38:14 +0700

    revised competitors Sky

--- main.py
+++ main.py
@@ -7,38 +7,136 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 
-#SKY
-sky=[
-    {"name":"Relocate", "url":"https://www.airbnb.co.uk/rooms/871040174926257448?source_impression_id=p3_1704450998_hM8uppOkkjhY8NdK&check_in=","line_color":"r"},
-     {"name":"Skyline","url":"https://www.airbnb.co.uk/rooms/893933063998940096?source_impression_id=p3_1704361618_0j1jiCryQcZYdVPl&check_in=","line_color":"g"},
-     {"name":"Bloomson","url":"https://www.airbnb.co.uk/rooms/1077423177222957828?source_impression_id=p3_1712150698_B9d6DRwQYwiSlIGc&check_in=","line_color":"b"},
-     {"name":"Tracy","url":"https://www.airbnb.co.uk/rooms/1002010727603598593?source_impression_id=p3_1712153527_mBbNtthTyJ1n0cuu&check_in=", "line_color": "orange"},
-     {"name":"Sky1008","url":"https://www.airbnb.co.uk/rooms/1046907771076731775?source_impression_id=p3_1704451469_3qliydVyTnmWa7FH&check_in=","line_color":"y"},
-     ]
+# SKY
+sky = [
+    {
+        "name": "Relocate",
+        "url": "https://www.airbnb.co.uk/rooms/871040174926257448?source_impression_id=p3_1704450998_hM8uppOkkjhY8NdK&check_in=",
+        "line_color": "r",
+    },
+    {
+        "name": "Skyline",
+        "url": "https://www.airbnb.co.uk/rooms/893933063998940096?source_impression_id=p3_1704361618_0j1jiCryQcZYdVPl&check_in=",
+        "line_color": "g",
+    },
+    {
+        "name": "Bloomson",
+        "url": "https://www.airbnb.co.uk/rooms/1077423177222957828?source_impression_id=p3_1712150698_B9d6DRwQYwiSlIGc&check_in=",
+        "line_color": "b",
+    },
+    {
+        "name": "SkyGarden Eli",
+        "url": "https://www.airbnb.co.uk/rooms/736788404696913896?source_impression_id=p3_1723431436_P3sSYsoqENOVl5H5&check_in=",
+        "line_color": "orange",
+    },
+    {
+        "name": "Can And Aleksandra",
+        "url": "https://www.airbnb.co.uk/rooms/22352403?source_impression_id=p3_1723431823_P3Vr9Hboh3fp-dn6&check_in=",
+        "line_color": "purple",
+    },
+    {
+        "name": "SkyGarden F",
+        "url": "https://www.airbnb.co.uk/rooms/1046907771076731775?source_impression_id=p3_1704451469_3qliydVyTnmWa7FH&check_in=",
+        "line_color": "yellow",
+    },
+    {
+        "name": "Xhorxhina",
+        "url": "https://www.airbnb.co.uk/rooms/45266901?source_impression_id=p3_1723432519_P3VDb-I6ci56atQL&check_in=",
+        "line_color": "pink",
+    },
+]
 
-#HUNTER
-hunter=[
-    {"name":"Charm", "url":"https://www.airbnb.co.uk/rooms/919494818502148128?source_impression_id=p3_1705312955_Qq3gYbrhzJIcQ90A&check_in=","line_color":"r"},
-     {"name":"Vintage","url":"https://www.airbnb.co.uk/rooms/901306741280213973?source_impression_id=p3_1705312818_1cWKDpsOurb3ssZD&check_in=","line_color":"g"},
-     {"name":"Oneil","url":"https://www.airbnb.co.uk/rooms/903131518446432064?source_impression_id=p3_1705311821_p9worQnaTtWB52AA&check_in=","line_color":"b"},
-     {"name":"Hunter8","url":"https://www.airbnb.co.uk/rooms/991919969842748351?source_impression_id=p3_1705313120_c%2Fm2yGmapv9ksHVg&check_in=","line_color":"y"}
-     ]
+# HUNTER
+hunter = [
+    {
+        "name": "Harry",
+        "url": "https://www.airbnb.co.uk/rooms/901306741280213973?source_impression_id=p3_1705312818_1cWKDpsOurb3ssZD&check_in=",
+        "line_color": "g",
+    },
+    {
+        "name": "Oneil",
+        "url": "https://www.airbnb.co.uk/rooms/903131518446432064?source_impression_id=p3_1705311821_p9worQnaTtWB52AA&check_in=",
+        "line_color": "b",
+    },
+    #  {"name":"Hunter8","url":"https://www.airbnb.co.uk/rooms/991919969842748351?source_impression_id=p3_1705313120_c%2Fm2yGmapv9ksHVg&check_in=","line_color":"y"},
+    {
+        "name": "Dilen",
+        "url": "https://www.airbnb.co.uk/rooms/612203853376860930?source_impression_id=p3_1715678296_x0bfS%2BYfuLm40ZcW&check_in=",
+        "line_color": "orange",
+    },
+    {
+        "name": "CStays",
+        "url": "https://www.airbnb.co.uk/rooms/1123044516463216828?source_impression_id=p3_1715678482_2p1XGU5JyleogkTO&check_in=",
+        "line_color": "purple",
+    },
+]
 
-prince=[
-     {"name":"James","url":"https://www.airbnb.co.uk/rooms/839049066394923811?source_impression_id=p3_1712155883_JxD%2FYJ282oelANG8&check_in=","line_color":"g"},
-     {"name":"John","url":"https://www.airbnb.co.uk/rooms/1035252707118091218?source_impression_id=p3_1711898457_i1f8UFewhDAgVuDS&check_in=","line_color":"b"},
-     {"name":"Anna","url":"https://www.airbnb.co.uk/rooms/41347455?source_impression_id=p3_1711898456_X8V6jGQbb5Mv9n4g&check_in=", "line_color": "orange"},
-     {"name":"Gianni","url":"https://www.airbnb.co.uk/rooms/24349122?source_impression_id=p3_1711898457_5FlY9H4NthjB8G8M&check_in=", "line_color": "purple"},
-     {"name":"Princes Sq","url":"https://www.airbnb.co.uk/rooms/1110055308129837928?source_impression_id=p3_1711899452_VLF2b6atGPBzhTWn&check_in=","line_color":"y"},
+prince = [
+    {
+        "name": "Ramos",
+        "url": "https://www.airbnb.co.uk/rooms/1160720252603364860?source_impression_id=p3_1712155883_JxD%2FYJ282oelANG8&check_in=",
+        "line_color": "g",
+    },
+    {
+        "name": "Haifi",
+        "url": "https://www.airbnb.co.uk/rooms/1129020937604624995?source_impression_id=p3_1711898457_i1f8UFewhDAgVuDS&check_in=",
+        "line_color": "b",
+    },
+    {
+        "name": "Hassan",
+        "url": "https://www.airbnb.co.uk/rooms/52406093?source_impression_id=p3_1711898456_X8V6jGQbb5Mv9n4g&check_in=",
+        "line_color": "orange",
+    },
+    {
+        "name": "Gianni",
+        "url": "https://www.airbnb.co.uk/rooms/841073783967208304?source_impression_id=p3_1711898457_5FlY9H4NthjB8G8M&check_in=",
+        "line_color": "purple",
+    },
+    {
+        "name": "Lyandah",
+        "url": "https://www.airbnb.co.uk/rooms/32623773?source_impression_id=p3_1717061367_sgv33kja84bav1HT&check_in=",
+        "line_color": "r",
+    },
+    #  {"name":"Princes Sq","url":"https://www.airbnb.co.uk/rooms/1110055308129837928?source_impression_id=p3_1711899452_VLF2b6atGPBzhTWn&check_in=","line_color":"y"},
+]
+
+bowden = [
+    {
+        "name": "Dominic",
+        "url": "https://www.airbnb.co.uk/rooms/1116057187762364001?source_impression_id=p3_1717739081_P3UYdriW0HcMgFZH&check_in=",
+        "line_color": "g",
+    },
+    {
+        "name": "Tim",
+        "url": "https://www.airbnb.co.uk/rooms/1143484057714978646?source_impression_id=p3_1717739648_P3hJs4odKBU093-G&check_in=",
+        "line_color": "b",
+    },
+    {
+        "name": "Capital",
+        "url": "https://www.airbnb.co.uk/rooms/1171602217690580795?source_impression_id=p3_1717739914_P3Q0fIyPfxJB2iI4&check_in=",
+        "line_color": "orange",
+    },
+    {
+        "name": "Ramona",
+        "url": "https://www.airbnb.co.uk/rooms/44127966?&source_impression_id=p3_1717740580_P3_8EIp9PPYPKIB8&check_in=",
+        "line_color": "purple",
+    },
+    {
+        "name": "Alice",
+        "url": "https://www.airbnb.co.uk/rooms/1161596523852226746?&source_impression_id=p3_1717740740_P3BlB0g2JGbY32sx&check_in=",
+        "line_color": "r",
+    },
 ]
 
 
 def getBnb(datalist):
-    graph_name=datalist.capitalize()
+    graph_name = datalist.capitalize()
     if datalist == "sky":
         datalist = sky
     elif datalist == "prince":
         datalist = prince
+    elif datalist == "bowden":
+        datalist = bowden
     else:
         datalist = hunter
     # datalist = sky
@@ -47,16 +145,16 @@ def getBnb(datalist):
     # arrivalDate = "12-05-2024"
     num_days = input("Number of days : ")
     # num_days = 4
-    min_nights=int(input("Minimum nigths : "))
+    min_nights = int(input("Minimum nigths : "))
     # min_nights = 4
-    arrivalDate = datetime.strptime(arrivalDate,"%d-%m-%Y")
+    arrivalDate = datetime.strptime(arrivalDate, "%d-%m-%Y")
     nights = timedelta(days=min_nights)
-    arrFileName=arrivalDate.strftime("%m-%d-%Y")
-    departureDate = arrivalDate+nights
+    arrFileName = arrivalDate.strftime("%d-%m-%Y")
+    departureDate = arrivalDate + nights
     graphDays = timedelta(days=int(num_days))
-    graphDepartureDate = arrivalDate+graphDays
+    graphDepartureDate = arrivalDate + graphDays
     departureDate = departureDate.strftime("%Y-%m-%d")
-    graphDepartureDate = graphDepartureDate.strftime("%m-%d-%Y")
+    graphDepartureDate = graphDepartureDate.strftime("%d-%m-%Y")
     arrivalDate = arrivalDate.strftime("%Y-%m-%d")
 
     options = Options()
@@ -64,26 +162,28 @@ def getBnb(datalist):
     options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
 
-    getRates(datalist,arrivalDate, departureDate,min_nights,num_days,driver)
-    
-    plt.rcParams['figure.figsize'] = [12, 7]    
+    getRates(datalist, arrivalDate, departureDate, min_nights, num_days, driver)
+
+    plt.rcParams["figure.figsize"] = [12, 7]
     plt.legend()
-    plt.xlabel("DATE",fontweight="bold")
-    plt.ylabel("RATE/NIGHT",fontweight="bold")
-    plt.title(f"Rate Comparison {graph_name} From {arrFileName} until {graphDepartureDate}",fontweight="bold")
-    plt.ylim(150,650)
-    plt.xticks(rotation=45)  
-    myFmt = mdates.DateFormatter('%m / %d')
+    plt.xlabel("DATE", fontweight="bold")
+    plt.ylabel("RATE/NIGHT", fontweight="bold")
+    plt.title(
+        f"Rate Comparison {graph_name} From {arrFileName} until {graphDepartureDate}",
+        fontweight="bold",
+    )
+    plt.ylim(150, 650)
+    plt.xticks(rotation=45)
+    myFmt = mdates.DateFormatter("%d / %m")
     plt.gca().xaxis.set_major_formatter(myFmt)
-    file_time= datetime.now().strftime("%H-%M-%S")
-    today_date = datetime.now().strftime("%m-%d-%Y")
-    plt.savefig(f"graph/{graph_name} {arrFileName} updated {today_date} {file_time}.png")
+    file_time = datetime.now().strftime("%H-%M-%S")
+    today_date = datetime.now().strftime("%d-%m-%Y")
+    plt.savefig(
+        f"graph/{graph_name} {arrFileName} updated {today_date} {file_time}.png"
+    )
     print("DONE")
     plt.show()
 
 
 choice = input("Enter 'sky' for Sky Garden or 'hunter' for Hunter House or 'prince' : ")
 getBnb(choice)
-
-
-
--- utils.py
+++ utils.py
@@ -10,36 +10,38 @@ from selenium.webdriver.support import expected_conditions as EC
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def addOneDay(element, min_nights):
-    arrivalDate = datetime.strptime(element,"%Y-%m-%d")
+    arrivalDate = datetime.strptime(element, "%Y-%m-%d")
     arrivalDate += timedelta(days=1)
     nights = timedelta(days=int(min_nights))
-    departureDate = arrivalDate+nights
+    departureDate = arrivalDate + nights
     departureDate = departureDate.strftime("%Y-%m-%d")
     arrivalDate = arrivalDate.strftime("%Y-%m-%d")
-    return {"arrivalDate":arrivalDate, "departureDate":departureDate}
+    return {"arrivalDate": arrivalDate, "departureDate": departureDate}
 
-def getRates(listProperty,arrivalDate,departureDate, min_nights, num_days, driver):
+
+def getRates(listProperty, arrivalDate, departureDate, min_nights, num_days, driver):
     min_nights = int(min_nights)
     for index in range(len(listProperty)):
         for key in listProperty[index]:
-            property_name=listProperty[index]["name"]
-            line_color=listProperty[index]["line_color"]
+            property_name = listProperty[index]["name"]
+            line_color = listProperty[index]["line_color"]
             if key == "url":
-                url=listProperty[index][key]
+                url = listProperty[index][key]
                 print("PROPERTY", property_name)
                 # print("LINE COLOR", line_color)
-                skyline_x=[]
-                skyline_y=[]
-                initArrDate=arrivalDate
-                initDepDate=departureDate
-                
+                skyline_x = []
+                skyline_y = []
+                initArrDate = arrivalDate
+                initDepDate = departureDate
+
                 for _ in range(int(num_days)):
                     print("ARRIVAL DATE", arrivalDate)
 
-                    try:      
-                        new_url=f"{url}{arrivalDate}&guests=4&adults=4&check_out={departureDate}"
-                        print("NEWURL",new_url)
+                    try:
+                        new_url = f"{url}{arrivalDate}&guests=4&adults=4&check_out={departureDate}"
+                        print("NEWURL", new_url)
                         driver.get(new_url)
                         # print("SKYLINE 2 BED 2 BATH")
                         # print("URL", url)
@@ -55,33 +57,37 @@ def getRates(listProperty,arrivalDate,departureDate, min_nights, num_days, drive
                         #     pass
                         try:
                             total_price = WebDriverWait(driver, 10).until(
-                                EC.presence_of_element_located((By.XPATH, "/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div[3]/div/section/div[2]/div/span[2]/span[1]/span")))
-                            total_price=total_price.get_attribute("innerHTML")
-                            total_price = total_price.replace("£","")
-                                 
-                            price_per_night=0
-                            if len(total_price) >3:
-                                total_price = total_price.replace(",","")
+                                EC.presence_of_element_located(
+                                    (
+                                        By.XPATH,
+                                        "/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div[3]/div/section/div[2]/div/span[2]/span[1]/span",
+                                    )
+                                )
+                            )
+                            total_price = total_price.get_attribute("innerHTML")
+                            total_price = total_price.replace("£", "")
+
+                            price_per_night = 0
+                            if len(total_price) > 3:
+                                total_price = total_price.replace(",", "")
                                 total_price = int(total_price.strip())
-                                price_per_night = total_price/min_nights
+                                price_per_night = total_price / min_nights
                             else:
                                 total_price = int(total_price.strip())
-                                price_per_night = total_price/min_nights
-                            
-                            print("Rate/night £",price_per_night)
+                                price_per_night = total_price / min_nights
+
+                            print("Rate/night £", price_per_night)
                             skyline_y.append(price_per_night)
-                            
+
                         except Exception as e:
                             print("EXCEPTIONM")
                             skyline_y.append(None)
-                                            
-                                                                     
+
                         # total_price = driver.find_element(By.XPATH, "/html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div[3]/div/section/div[2]/div/span[2]/span[1]/span")
                         # print("TOTAL PRICE", total_price.text)
-                        
-                        
+
                     except Exception as e:
-                        print("EXECEPTION",e)
+                        print("EXECEPTION", e)
                         skyline_y.append(None)
                     finally:
                         # print("Arrival", arrivalDate)
@@ -90,32 +96,39 @@ def getRates(listProperty,arrivalDate,departureDate, min_nights, num_days, drive
                         result = addOneDay(arrivalDate, min_nights)
                         arrivalDate = result["arrivalDate"]
                         departureDate = result["departureDate"]
-                       
+
                 arrivalDate = initArrDate
                 departureDate = initDepDate
-                print(property_name,skyline_x, skyline_y)
-            
-                x_data = [ date.fromisoformat(i) for i in skyline_x]
-                x_data=np.array(x_data)
-               
+                print(property_name, skyline_x, skyline_y)
+
+                x_data = [date.fromisoformat(i) for i in skyline_x]
+                x_data = np.array(x_data)
+
                 y_data = np.array(skyline_y).astype(np.double)
                 # plt.ylim(min(y_data),500)
                 sky_mask = np.isfinite(y_data)
-                plt.plot_date(x_data[sky_mask],y_data[sky_mask], line_color,marker="o", label=property_name)
+                plt.plot_date(
+                    x_data[sky_mask],
+                    y_data[sky_mask],
+                    line_color,
+                    marker="o",
+                    label=property_name,
+                )
 
-                fields = ["Date", "Rate/night"]
-                merged_list=[]
-                for i in range(len(skyline_x)):
-                    thelist=[]
-                    thelist.append(skyline_x[i])
-                    thelist.append(skyline_y[i])
-                    merged_list.append(thelist)
-                filename = f"csv/{property_name}.csv"
-                # writing to csv file
-                with open(filename, 'w') as csvfile:
-                    # creating a csv writer object
-                    csvwriter = csv.writer(csvfile)
-                    # writing the fields
-                    csvwriter.writerow(fields)
-                    # writing the data rows
-                    csvwriter.writerows(merged_list)
+                # WRITE TO CSV
+                # fields = ["Date", "Rate/night"]
+                # merged_list=[]
+                # for i in range(len(skyline_x)):
+                #     thelist=[]
+                #     thelist.append(skyline_x[i])
+                #     thelist.append(skyline_y[i])
+                #     merged_list.append(thelist)
+                # filename = f"csv/{property_name}.csv"
+                # # writing to csv file
+                # with open(filename, 'w') as csvfile:
+                #     # creating a csv writer object
+                #     csvwriter = csv.writer(csvfile)
+                #     # writing the fields
+                #     csvwriter.writerow(fields)
+                #     # writing the data rows
+                #     csvwriter.writerows(merged_list)
